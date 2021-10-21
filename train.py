092cde346f355cf4a51acb45bbf1eb44345bd086
haakwi <hasanakhuy@msn.com>
2021-10-21 14:36:10 +0200

    Test train

--- train.py
+++ train.py
@@ -58,6 +58,7 @@ if __name__ == "__main__":
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
+    # write logs
     mlflow.sklearn.autolog()
 
     # host = "http://35.241.233.235/mlflow/"
