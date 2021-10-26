ff9f78ecd49a292dc4417efafd3e2b5058776ce3
haakwi <hasanakhuy@msn.com>
2021-10-27 00:13:11 +0200

    Add documentation

--- load_test/locustfile.py
+++ load_test/locustfile.py
@@ -10,7 +10,7 @@ class WebsiteUser(HttpUser):
     # random time between 1 and 3 seconds
     waitTime = between(1, 1)
 
-    @task
+    @ task
     def index_page(self):
         # self.client.get(url='/#home')
 
